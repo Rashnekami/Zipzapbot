fdadeffca9fb110f2dded85d454060fd9797557f
Rashnekami <149558475+Rashnekami@users.noreply.github.com>
2026-08-27 01:13:45 -0300

    Add clean colorful Pterodactyl console

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import {startPanel} from "./panel.js";
 import {startBot} from "./bot.js";
+import {printBanner} from "./logger.js";
+printBanner();
 startPanel();
 startBot().catch(error=>{console.error(error);process.exitCode=1});
